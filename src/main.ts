5c79285a4d982a02c0bc70f0cd23b452629ad43c
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-10-02 15:16:54 -0300

    actualizando relaciones

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
