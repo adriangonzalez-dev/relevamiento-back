7dfe69f29d2572e2f75a1049b983883b672a6bb5
adriang1990 <adriang@siainteractive.com>
2023-12-01 15:55:26 -0300

    cors fixed

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:5173'],
   });
   app.useGlobalPipes(
     new ValidationPipe({
