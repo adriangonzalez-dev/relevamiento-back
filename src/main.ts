64587857f988d710af6410640c675d8f76a1e2bd
adriang1990 <adriang@siainteractive.com>
2023-12-01 16:27:57 -0300

    fix 2 cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'https://dev--glittering-rabanadas-e31e1d.netlify.app/',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
   });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
